bef9c5876b019a86d6fe2d6a0adf2052f7eab28d
elj40 <elaijoubert@gmail.com>
2023-10-22 16:11:10 +0200

    Made car better to drive

--- Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
+++ Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
@@ -9,6 +9,7 @@ public class CameraFollow : MonoBehaviour {
 	public float followSpeed = 2;
 	[Range(1, 10)]
 	public float lookSpeed = 5;
+	public Vector3 cameraOffset;
 	Vector3 initialCameraPosition;
 	Vector3 initialCarPosition;
 	Vector3 absoluteInitCameraPosition;
@@ -22,14 +23,21 @@ public class CameraFollow : MonoBehaviour {
 	}
 
 	void FixedUpdate()
-	{
+	{	
+
+
+
 		//Look at car
 		Vector3 _lookDirection = (new Vector3(carTransform.position.x, carTransform.position.y, carTransform.position.z)) - transform.position;
 		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
 		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
 
-		//Move to car
-		Vector3 _targetPos = absoluteInitCameraPosition + carTransform.transform.position;
+
+		Vector3 cameraOffsetY = new Vector3(0,cameraOffset.y,0);
+		float cameraOffsetZ = cameraOffset.z;
+		//Move to car 
+		//Vector3 _targetPos = absoluteInitCameraPosition + carTransform.transform.position;
+		Vector3 _targetPos = carTransform.transform.position + carTransform.rotation * Vector3.forward * -cameraOffsetZ + cameraOffsetY;
 		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
 
 	}
